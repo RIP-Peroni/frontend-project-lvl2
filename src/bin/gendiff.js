40f239f3e5c18cd8f1f0b16ad66fa5c204f61c60
RIP-Peroni <epih96@mail.ru>
2020-01-05 21:56:04 +0300

    2 step

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -2,4 +2,7 @@
 const program = require('commander');
 program
   .version('0.0.1')
+  .description('Compares two configuration files and shows a difference.')
+  .usage('[options] <firstConfig> <secondConfig>')
+  .option('-f, --format [type]', 'Output format');
 program.parse(process.argv);
